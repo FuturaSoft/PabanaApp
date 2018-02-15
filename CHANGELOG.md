9f1e481229df4519e2f944abb33d07015aea1a86
t.forax <t.forax@futurasoft.fr>
2018-02-15 21:04:45 +0100

    Update changelog and configuration for app version Update date in LICENSE

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGELOG
 
+## 1.1.0 - 05/02/2018
+
+FIX : [Global] : Respect PSR-1 and PSR-2
+FIX : [View] : Use simple open tag (<?=)
+
 ## 1.0.0 - 05/02/2018
 
 Initial release
--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	"application.name" => "Pabana skeleton app",
-    "application.version" => "1.0.0",
+    "application.version" => "1.1.0",
 	"application.env" => "dev",
 	"debug.level" => "E_ALL"
 );
